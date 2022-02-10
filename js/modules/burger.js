4445ec6a9f73eb893f407579615bb56d2f43550e
helpMeRise <rises128@gmail.com>
2022-02-10 20:38:57 +0200

    Выполнил дополнительное задание

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -6,4 +6,11 @@ export const burger = () => {
   headerMenuButton.addEventListener('click', () => {
     headerMenu.classList.toggle('header__menu_active');
   });
+
+  document.addEventListener('click', e => {
+    if (!e.target.closest('.header__menu') && e.target !== headerMenuButton ||
+      e.target.closest('.header__link')) {
+      headerMenu.classList.remove('header__menu_active');
+    }
+  });
 };
